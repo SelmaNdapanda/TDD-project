c3dba14d9e5742307a2c2bf6f4e6ac161cbbaf8a
Selma <selmandapanda@yahoo.com>
2023-01-21 15:23:14 +0200

    Test the factorial method

--- _tests_/solver_spec.rb
+++ _tests_/solver_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../solver'
 
 describe Solver do
-    let(:solver) { Solver.new }
+  let(:solver) { Solver.new }
+
+  describe '#factorial' do
+    it 'returns the factorial of a positive number' do
+      expect(solver.factorial(5)).to eq(120)
+    end
+    
+    it 'raises an error when given a negative number' do
+      expect { solver.factorial(-5) }.to raise_error(StandardError, 'NegativeNumberFactorialError')
+    end
+    
+    it 'Returns 1 when argument passed is 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+  end
 end
--- solver.rb
+++ solver.rb
@@ -1,2 +1,4 @@
 class Solver
+  def factorial(num)
+  end
 end
